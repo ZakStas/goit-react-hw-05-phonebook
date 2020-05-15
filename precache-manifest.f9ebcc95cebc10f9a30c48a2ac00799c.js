7beec3e3999f4857b97c7d9c7e3a7143c595d68a
ZakStas <stanislavzakarliuka1989@gmail.com>
2020-05-15 17:10:45 +0300

    Updates

---
+++ precache-manifest.f9ebcc95cebc10f9a30c48a2ac00799c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed40063c4027388db073fb1ec6e0481",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "384e4b49df9e47095097",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.3a983aac.chunk.css"
+  },
+  {
+    "revision": "fb738f04794e22d559bc",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.81fe8906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.81fe8906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384e4b49df9e47095097",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.7647f94e.chunk.js"
+  },
+  {
+    "revision": "57a297c90cd08d9bd131",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.3e27e864.js"
+  }
+]);
